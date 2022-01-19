1ba18c907d66792c615d746ad202bd07ee03d960
OlehPotap <potap.oleh.v@gmail.com>
2022-01-19 16:46:43 +0200

    hw08task02

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,42 +6,42 @@ function getRandomHexColor() {
 
 // ==================================Способ 1================================================
 
-const buttonEls = document.querySelectorAll('button');
-
-let bgChangerInterval = null;
-
-buttonEls.forEach(el => {
-  el.addEventListener('click', () => {
-    el.setAttribute('disabled', 'disabled');
-    if (el.hasAttribute('data-start')) {
-      bgChangerInterval = setInterval(() => {
-        document.body.style.backgroundColor = getRandomHexColor();
-      }, 1000);
-      document.querySelector('button[data-stop]').removeAttribute('disabled');
-    } else {
-      clearInterval(bgChangerInterval);
-      document.querySelector('button[data-start]').removeAttribute('disabled');
-    }
-  });
-});
-
-// ==================================Способ 2================================================
-
-// const buttonStartEl = document.querySelector('button[data-start]');
-// const buttonStoptEl = document.querySelector('button[data-stop]');
+// const buttonEls = document.querySelectorAll('button');
 
 // let bgChangerInterval = null;
 
-// buttonStartEl.addEventListener('click', () => {
-//   bgChangerInterval = setInterval(() => {
-//     document.body.style.backgroundColor = getRandomHexColor();
-//   }, 1000);
-//   buttonStartEl.setAttribute('disabled', 'disabled');
-//   buttonStoptEl.removeAttribute('disabled');
+// buttonEls.forEach(el => {
+//   el.addEventListener('click', () => {
+//     el.setAttribute('disabled', 'disabled');
+//     if (el.hasAttribute('data-start')) {
+//       bgChangerInterval = setInterval(() => {
+//         document.body.style.backgroundColor = getRandomHexColor();
+//       }, 1000);
+//       document.querySelector('button[data-stop]').removeAttribute('disabled');
+//     } else {
+//       clearInterval(bgChangerInterval);
+//       document.querySelector('button[data-start]').removeAttribute('disabled');
+//     }
+//   });
 // });
 
-// buttonStoptEl.addEventListener('click', () => {
-//   clearInterval(bgChangerInterval);
-//   buttonStoptEl.setAttribute('disabled', 'disabled');
-//   buttonStartEl.removeAttribute('disabled');
-// });
+// ==================================Способ 2================================================
+
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStoptEl = document.querySelector('button[data-stop]');
+
+let bgChangerInterval = null;
+
+buttonStartEl.addEventListener('click', () => {
+  bgChangerInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStartEl.setAttribute('disabled', 'disabled');
+  buttonStoptEl.removeAttribute('disabled');
+});
+
+buttonStoptEl.addEventListener('click', () => {
+  clearInterval(bgChangerInterval);
+  buttonStoptEl.setAttribute('disabled', 'disabled');
+  buttonStartEl.removeAttribute('disabled');
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,83 @@
+'use strict';
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+import Notiflix from 'notiflix';
+
+// Функция форматирования времени
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
+
+// Функция конвертации милисекунд в адекватную дату
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// Обращаемся к елементам на странице
+
+const startButtonEl = document.querySelector('button[data-start]');
+const dateInputEl = document.querySelector('#datetime-picker');
+
+// с помощью флетпикра позволяем пользователю выбрать любую дату в будущем, в противном случае кнопка начать отчет не активна
+
+let now = new Date();
+startButtonEl.setAttribute('disabled', 'disabled');
+
+flatpickr(dateInputEl, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (Number(now) > Number(selectedDates[0])) {
+      clearInterval(setIntervalForTimer);
+      document.querySelector('span[data-days]').textContent = addLeadingZero('0');
+      document.querySelector('span[data-hours]').textContent = addLeadingZero('0');
+      document.querySelector('span[data-minutes]').textContent = addLeadingZero('0');
+      document.querySelector('span[data-seconds]').textContent = addLeadingZero('0');
+      startButtonEl.setAttribute('disabled', 'disabled');
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startButtonEl.removeAttribute('disabled');
+    }
+  },
+});
+
+// По клику на кнопкустарт если она активна записывает в переменную дедлайн значение даты выбранное пользователем
+
+let deadline = null;
+let setIntervalForTimer = null;
+
+startButtonEl.addEventListener('click', event => {
+  setIntervalForTimer = setInterval(() => {
+    deadline = new Date(dateInputEl.value);
+    const convertedDate = convertMs(Number(deadline) - Number(new Date()));
+    document.querySelector('span[data-days]').textContent = addLeadingZero(convertedDate.days);
+    document.querySelector('span[data-hours]').textContent = addLeadingZero(convertedDate.hours);
+    document.querySelector('span[data-minutes]').textContent = addLeadingZero(
+      convertedDate.minutes,
+    );
+    document.querySelector('span[data-seconds]').textContent = addLeadingZero(
+      convertedDate.seconds,
+    );
+  }, 1000);
+});
